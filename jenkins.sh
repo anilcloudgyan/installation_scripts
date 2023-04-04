cf009bc4872225148f121573adbcb368435e868e
anilcloudgyan <99344622+anilcloudgyan@users.noreply.github.com>
2023-04-04 19:08:28 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
-sudo apt install git
-cat /var/lib/jenkins/secrets/initialAdminPassword
-sudo service jenkins status
+#!/bin/bash
 
+sudo apt update -y
+
+sudo apt install default-jre -y
+
+java -version
+
+sudo apt update -y
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update -y
+
+sudo add-apt-repository universe -y
+
+sudo apt-get install jenkins -y
+
+sudo service jenkins start
 
